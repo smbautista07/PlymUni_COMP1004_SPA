f5779cbd4a813241f71f1e663a0b70deb4325458
seanb-313 <18nbautistas@dhsb.org>
2026-02-09 12:36:22 +0000

    Display handler and rectangle object
    Moved canvas creation and handling into a class for encapsulation. Created a rectangle class with its constructor arguments being an object with self explanatory properties (example on line 40) and a way for the displayHandler to draw it.

--- pong.js
+++ pong.js
@@ -1,13 +1,42 @@
 "use strict";
 
-function initialise()
+class displayHandler
 {
-    const gameDisplay = document.createElement("canvas");
-    gameDisplay.width = 960;
-    gameDisplay.height = 540;
-    gameDisplay.id = "display";
-    document.body.appendChild(gameDisplay);
-    console.log("connected");
+    constructor()
+    {
+        this.gameCanvas = document.createElement("canvas");
+        this.gameCanvas.width = 960;
+        this.gameCanvas.height = 540;
+        this.gameCanvas.id = "display";
+        document.body.appendChild(this.gameCanvas);
+        
+        this.ctx = this.gameCanvas.getContext("2d");
+    }
+
+    drawRect(rect_obj)
+    {
+        this.ctx.fillRect(rect_obj.x,rect_obj.y,rect_obj.width,rect_obj.height);
+    }
+
 }
 
-initialise();
+class rectangle
+{
+    constructor({height, width, x, y})
+    {
+        this.height = height;
+        this.width = width;
+        this.x = x;
+        this.y = y;
+    }
+}
+
+var pongDisplay;
+function initialise()
+{
+    pongDisplay = new displayHandler();
+}
+
+var r = new rectangle({height:100, x: 32, y:100, width:32});
+
+pongDisplay.drawRect(r);
