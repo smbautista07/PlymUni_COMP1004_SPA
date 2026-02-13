0b14628d2667abe7a8036716b9c0e427530c3311
smbautista07 <seanmabautista@gmail.com>
2026-02-13 19:09:40 +0000

    removed alignment, x and y parameters for rectangle class
    The alignment system seems unnecessary since it's only used once for the initial position of the gameObjects. x and y properties can be set any time after initialisation, and a rectangle object should be able to exist without being at any location.

--- pong.js
+++ pong.js
@@ -57,89 +57,26 @@ class displayHandler
 
 class rectangle
 {
-    constructor({height, width, x, y, screenAlignmentX="L", screenAlignmentY="T", selfAlignmentX, selfAlignmentY})
+    constructor({height, width})
     {
         this.height = height;
         this.width = width;
-        var offsetX = 0;
-        var offsetY = 0;
         this.speedX = 0;
         this.speedY = 0;
-
-
+        
         gameObjectHandler.gameObjects.add(this);
-
-
-        if (selfAlignmentX === undefined)
-        {
-            selfAlignmentX = screenAlignmentX;
-        } 
-        if (selfAlignmentY === undefined)
-        {
-            selfAlignmentY = screenAlignmentY;
-        } 
-
-        switch (selfAlignmentY)
-        {
-            case "T":
-                break;
-            case "B":
-                offsetY += -height;
-                break;
-            case "M":
-                offsetY += -height/2;
-                break;
-            default:
-                console.error("Invalid selfAlignmentY argument");
-                offsetY += -9999;
-        }
-
-        switch (selfAlignmentX)
-        {
-            case "L":
-                break;
-            case "R":
-                offsetX += -width;
-                break;
-            case "M":
-                offsetX += -width/2;
-                break;
-            default:
-                console.error("Invalid selfAlignmentX argument");
-                offsetX += -9999;
-        }
-
-        switch (screenAlignmentY)
-        {
-            case "T":
-                break;
-            case "B":
-                y = displayHandler.getHeight() - y;
-                break;
-            default:
-                console.error("Invalid screenAlignment argument");
-                y = -9999;
-        }
-
-        switch (screenAlignmentX)
-        {
-            case "L":
-                break;
-            case "R":
-                x = displayHandler.getWidth() - x;
-                break;
-            default:
-                console.error("Invalid screenAlignment argument");
-                x = -9999;
-        }
-
-        this.setPosition({x: x+offsetX, y: y+offsetY});
     }
 
     setPosition({x,y})
     {
-        this.x = x;
-        this.y = y;
+        if (x !== undefined)
+        {
+            this.x = x;    
+        } 
+        if (y !== undefined)
+        {
+            this.y = y;    
+        } 
     }
 }
 
@@ -170,21 +107,20 @@ function initialise()
 {
     displayHandler.createDisplay({width:960, height:540});    
     
-    pongBall = new rectangle({height:20, width:20,x:0,y:0, screenAlignmentX:"L"});
-    leftPaddle = new rectangle({height:100, width:10, x:50, y:displayHandler.getHeight()/2,selfAlignmentY:"M"});
-    rightPaddle = new rectangle({height:100, width:10, x:50, y:displayHandler.getHeight()/2, screenAlignmentX:"R",selfAlignmentY:"M"});
+    pongBall = new rectangle({height:20, width:20});
+    leftPaddle = new rectangle({height:100, width:10});
+    rightPaddle = new rectangle({height:100, width:10});
 
-    // displayHandler.redrawAll();
+    pongBall.setPosition({x:0, y:0});
+    leftPaddle.setPosition({x:50, y:(displayHandler.getHeight()-leftPaddle.width)/2});
+    rightPaddle.setPosition({x:displayHandler.getWidth() - rightPaddle.width-50, y:(displayHandler.getHeight()-rightPaddle.height)/2});
+
     pongBall.speedX=10;
     leftPaddle.speedY=10;
 
-    console.log("Hello");
 
-    // gameObjectHandler.positionUpdate(pongBall);
-    // gameObjectHandler.outputObj();
-    // console.log(gameObjectHandler.gameObjects);
     setInterval(update, 30);
-    // displayHandler.redrawAll();
+    console.log(gameObjectHandler.gameObjects);
 }
 
 
